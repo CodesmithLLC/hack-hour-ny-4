164650e170c9086283080e7c96638f2288e803ce
Wontae Han <wontaeh@gmail.com>
2018-05-04 09:56:02 -0400

    First hack-hour commit

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -14,7 +14,18 @@
 */
 
 function drawStairs(n) {
-
+    let spaces = '', asterisks = '';
+    for(let i = 1; i <= n; i++) {
+        for(let j = n-i; j > 0; j--) {
+            spaces += ' ';
+        }
+        for(let k = 0; k < i; k++) {
+            asterisks += '*';
+        }
+        console.log(spaces + asterisks);
+        spaces = '';
+        asterisks = '';
+    }
 }
 
 
