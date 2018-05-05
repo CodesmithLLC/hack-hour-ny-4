8eaf85b7fbb8d1d7f0d4a240e7a5dec6c6602ce6
Wontae Han <wontaeh@gmail.com>
2018-05-05 10:01:17 -0400

    Fist draft of mode-mean.js

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,39 @@
 
 
 function modemean(array) {
+  // find mean
+  let mean = array.reduce(function(acc, curr) {
+    return acc + curr;
+  }) / array.length;
+  mean = Math.floor(mean);
 
+  // count each element in array
+  let count = array.reduce(function(acc, curr) {
+    if(acc[curr]) {
+      acc[curr]++;
+    } else {
+      acc[curr] = 1;
+    }
+    return acc;
+  }, {});;
+
+  let most = 2;
+  let mode;
+  for(let prop in count) {
+    if(count[prop] >= most) {
+      if(mode) {
+        if(mode < prop) {
+          most = count[prop];
+          mode = Number(prop);
+        }
+      } else {
+        most = count[prop];
+        mode = Number(prop);
+      }
+    }
+  }
+  // console.log('mean: ',mean,' mode: ',mode);
+  return mean === mode;
 }
 
 module.exports = modemean;
