9e28d207ce96e9c7fc3af7cf4d12c0b9d1fcd2c6
Mahfuz Kabir <mahfuzk@gmai.com>
2018-05-05 11:16:39 -0400

    Completed Hack Hour #3

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -9,9 +9,25 @@
  *
  */
 
-
+// const test = [1, 15, 15, 16, 21, 21];
 function modemean(array) {
+  const mean = array.reduce(
+    (prev, curr) => Math.floor((prev + curr) / array.length),
+    0
+  );
+  const modeCounter = array.reduce((modeHash, freq) => {
+    if (freq in modeHash) modeHash[freq]++;
+    else modeHash[freq] = 1;
+    return modeHash;
+  }, {});
+  const highestCount = Math.max(...Object.values(modeCounter));
+  const highestFreq = [];
+  for (const count in modeCounter) {
+    if (modeCounter[count] === highestCount) highestFreq.push(count);
+  }
 
+  return mean === Math.max(...highestFreq);
 }
+// console.log(modemean(test));
 
 module.exports = modemean;
