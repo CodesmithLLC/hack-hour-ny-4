071ba5fa43d370d97ef0907c54758c70a2166f4e
Shahrukh <jalilshahrukh@gmail.com>
2018-05-05 10:29:03 -0400

    Mode-mean complete.

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,34 @@
 
 
 function modemean(array) {
-
+    var sum = array.reduce(function(accum, curr) { 
+        accum += curr; 
+        return accum; 
+      }, 0); 
+  
+      var mean = Math.floor(sum/array.length); 
+      
+      var mode = array.reduce(function(accum, curr, index) {
+          var occurence = 0;  
+          if (!accum[curr]) { 
+            occurence += 1; 
+            accum[curr] = occurence; 
+          } else if (accum.hasOwnProperty(curr)) {  
+            accum[curr]++; 
+          }//end if else 
+          return accum;
+      }, {}); 
+  
+      var max = 0;
+      var maxProp = 0; 
+      for(var prop in mode) {
+        if(mode[prop] > max) { 
+          max = mode[prop];
+          maxProp = prop;
+        }//end if  
+      }//end for
+  
+      return maxProp === 'mean';  
 }
 
 module.exports = modemean;
