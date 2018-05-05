9621c7bfbfe7a005c6f1e1a6dc4049c0dde93556
Alejandro R <Alexrom789@Gmail.com>
2018-05-05 10:04:01 -0400

    finished hack hour 5/5/18

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,36 @@
 
 
 function modemean(array) {
-
+    let mean = 0;
+    let mode = array[0];
+    let count = 1;
+    var modeMap = {};
+    for(let i = 0; i < array.length; i++){
+         mean += array[i];
+         let current = array[i];
+         if (modeMap[current] == null){
+             modeMap[current] = 1;
+         } else { 
+             modeMap[current]++;
+         }
+            if (modeMap[current] > count){
+                 mode = current;
+                 count = modeMap[current]
+             }
+         }
+    mean = Math.floor(mean / array.length);
+    console.log(mean);
+    console.log(mode);
+    if (mean === mode){
+       return true;
+    }
+    return false;
+    
 }
+
+
+console.log(modemean([3,2,4,1,2,2,4]));
+
+
 
 module.exports = modemean;
