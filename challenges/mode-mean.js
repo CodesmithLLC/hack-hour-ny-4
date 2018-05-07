621f29929c5033107cae34d1573f83d22fb0d290
Mahfuz Kabir <mahfuzk@gmai.com>
2018-05-07 00:18:17 -0400

    Corrected Code

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -9,21 +9,39 @@
  *
  */
 
-// const test = [1, 15, 15, 16, 21, 21];
+// const modeCounter = array.reduce((modeHash, freq) => {
+//   modeHash[freq] = (modeHash[freq] || 0) + 1;
+//   return modeHash;
+// }, {});
+// console.log(modeHash);
+
+// const modeMax = function mode(arr) {
+//   return arr.reduce(
+//     (current, item) => {
+//       current.numMapping[item] = (current.numMapping[item] || 0) + 1;
+//       const val = current.numMapping[item];
+//       if (val >= current.greatestFreq && item >= current.mode) {
+//         current.greatestFreq = val;
+//         current.mode = item;
+//       }
+//       return current;
+//     },
+//     { mode: null, greatestFreq: 0, numMapping: {} }
+//   ).mode;
+// };
+
+// const test = [1, 14, 14, 16, 21, 21];
+
 function modemean(array) {
   const mean = Math.floor(array.reduce((sum, val) => sum + val) / array.length);
-  const modeCounter = array.reduce((modeHash, freq) => {
-    if (freq in modeHash) modeHash[freq]++;
-    else modeHash[freq] = 1;
-    return modeHash;
-  }, {});
+  const modeCounter = {};
+  for (const num of array) modeCounter[num] = (modeCounter[num] || 0) + 1;
   const highestCount = Math.max(...Object.values(modeCounter));
   const highestFreq = [];
-  for (const count in modeCounter) {
+  for (const count in modeCounter)
     if (modeCounter[count] === highestCount) highestFreq.push(count);
-  }
-  return Math.floor(mean) === Math.max(...highestFreq);
+  return mean === Math.max(...highestFreq);
+  // return mean === modeMax(array);
 }
 // console.log(modemean(test));
-
-module.exports = modemean;
+// module.exports = modemean;
