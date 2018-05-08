fc12d4d09b4e8041c987ffeeea3cd560bdc8a08a
Wontae Han <wontaeh@gmail.com>
2018-05-08 09:46:38 -0400

    Update kth-to-last-node.js
    When kth value is not existing, return undefined.`

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -34,7 +34,7 @@ function kthToLastNode(k, head) {
   for(let i = 0; i < n; i++) {
     result = result.next;
   }
-  return result.value;
+  if(result) return result.value;
 }
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
