e827a3f788a6e3e267828b12472cdcfc43d8a794
Shahrukh <jalilshahrukh@gmail.com>
2018-05-15 10:10:56 -0400

    Balance Parens.

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -24,8 +24,65 @@
  *
  */
 
-function balancedParens(input){
-
-}
+function evenArr(input) { 
+    if (input.length % 2 === 0) {
+      let firstHalf = input.slice(0, input.length/2); 
+      let secondHalf = input.slice(input.length/2, input.length); 
+  
+      let secondHalfCheck = ''; 
+  
+      let index = firstHalf.length; 
+      while (index > -1) { 
+        switch (firstHalf[index]) { 
+          case '(': 
+            secondHalfCheck += ')';
+          break; 
+          case '{': 
+            secondHalfCheck += '}'; 
+          break;
+          case '[': 
+            secondHalfCheck += ']'; 
+          break;
+        }//end switch
+        index--; 
+      }//end while
+  
+      if(secondHalf === secondHalfCheck) {return true;}
+    }//end if
+  }
+  
+  function balancedParens(input) {
+    let nextTo = [];  
+    let index = 0; 
+    while (index < input.length) { 
+      switch (input[index]) { 
+        case '(':
+          if (input.indexOf(')') === -1) {return false;}
+          if (input.indexOf(')') === index+1) {nextTo.push(true)}
+          console.log('(', input.indexOf('(')); 
+          console.log(')', input.indexOf(')')); 
+          break; 
+        case '{':
+          if (input.indexOf('}') === -1) {return false;}
+          if (input.indexOf('}') === index+1) {nextTo.push(true);}
+          console.log('{', input.indexOf('{')); 
+          console.log('}', input.indexOf('}')); 
+          break; 
+        case '[': 
+          if (input.indexOf(']') === -1) {return false;}
+          if (input.indexOf(']') === index+1) {nextTo.push(true);}
+          console.log('[', input.indexOf('['));
+          console.log(']', input.indexOf(']')); 
+          break;
+      }//end switch 
+      index++; 
+    }//end while
+  
+    if (nextTo.includes(false)) {
+      return false;
+    } else if (evenArr(input) === true) { 
+      return true; 
+    }//end if
+  }
 
 module.exports = balancedParens;
