4d2cfc0405f87f328dbcbc191bd7048b81a58b98
Jeffrey Schrock <rhythmmagi@gmail.com>
2018-05-15 14:15:51 +0000

    balanced-parens finished check for ()

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -3,10 +3,10 @@
  * the parentheses are balanced and false otherwise.
  *
  * Example:
- *   balancedParens('(');  // false
- *   balancedParens('()'); // true
- *   balancedParens(')(');  // false
- *   balancedParens('(())');  // true
+ *   balancedParens( '(' );  // false
+ *   balancedParens( '()' ); // true
+ *   balancedParens( ')(' );  // false
+ *   balancedParens( '(())' );  // true
  *
  * Step 2:
  *   make your solution work for all types of brackets
@@ -25,7 +25,36 @@
  */
 
 function balancedParens(input){
-
+  let stringArr = input.split('');
+  let checked = [];
+  for(let i = 0; i < stringArr.length; i++){
+    checked.push(stringArr[i]);
+  }
+  return checkArray(checked)
 }
 
+function checkArray(array){
+  let open = [];
+  for(let i = 0; i < array.length; i++){
+    if(array[i] === '(') open[i] = array[i];
+    if(array[i] === ')') {
+      if (open.includes('(')){
+        open.pop(open.indexOf('('));
+      } 
+    }
+  }
+  // console.log(open.length)
+  // console.log(open);
+  let length = open.length
+  // console.log('length', length);
+  if(length === 0) console.log('true');
+  else console.log('false');
+}
+
+
 module.exports = balancedParens;
+
+// balancedParens( ')(' );
+// balancedParens('()');
+// balancedParens( '(())' );
+// balancedParens( ')(' );
