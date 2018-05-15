5b2fe2bd173408499bec703109785878eeb8e0c3
Alejandro R <Alexrom789@Gmail.com>
2018-05-15 10:05:56 -0400

    Attempted challenge

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -24,8 +24,47 @@
  *
  */
 
-function balancedParens(input){
+// function balancedParens(input){
+// let leftCount = 0;
+// let rightCount = 0;
+// let inputArr = input.split('');
+//     for (let i = 0; i < inputArr.length;i++){
+//         if (inputArr[i] === '('){
+//         leftCount++;
+//         }
+//     }
+//     for (let j = 0; j < inputArr.length;j++){
+//         if (inputArr[j] === ')'){
+//         rightCount++;
+//         }
+//     }
+// if (leftCount === rightCount) return true;
+//     return false;
+// //console.log(balanceCount);
+// }
 
-}
+///
+
+
+function balancedParens(input) {
+    const stack = [];
+    const openParens = { '{': '}', '[': ']', '(': ')' };
+    const closedParens = { '}': true, ']': true, ')': true };
+    
+    for (let i = 0; i < input.length; i ++) {
+      if (openParens[input[i]]) {
+        stack.push(input[i]);
+      } else if (closedParens[input[i]]) {
+        if (openParens[stack.pop()] !== input[i]) return false;
+      }
+    }
+    
+    return stack.length === 0;
+  };
+
+  console.log(balancedParens('(())'));
+  console.log(balancedParens(')('));
+  console.log(balancedParens('[({})]'));  
+  console.log(balancedParens('[(]{)}')); 
 
 module.exports = balancedParens;
