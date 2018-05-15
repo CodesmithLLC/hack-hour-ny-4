85690e18aca6886dbe96d5f9241b81886773b143
Shafayat Alam <shafayatalam05@gmail.com>
2018-05-15 10:02:16 -0400

    Completed HH challenge

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -1,31 +1,26 @@
-/*
- * write a function that takes a string of text and returns true if
- * the parentheses are balanced and false otherwise.
- *
- * Example:
- *   balancedParens('(');  // false
- *   balancedParens('()'); // true
- *   balancedParens(')(');  // false
- *   balancedParens('(())');  // true
- *
- * Step 2:
- *   make your solution work for all types of brackets
- *
- * Example:
- *  balancedParens('[](){}'); // true
- *  balancedParens('[({})]');   // true
- *  balancedParens('[(]{)}'); // false
- *
- * Step 3:
- * ignore non-bracket characters
- * balancedParens(' var wow  = { yo: thisIsAwesome() }'); // true
- * balancedParens(' var hubble = function() { telescopes.awesome();'); // false
- *
- *
- */
-
 function balancedParens(input){
+  let chars = input.split('').filter((char) => {
+    if (char === '(' || char === ')' || char === '[' || char === ']'|| char === '{' || char === '}') return char;
+  });
 
+  let parens = parenMapper(chars);
+  let output = true;
+  
+  for (let key in parens) {
+    if (parens[key] % 2 !== 0) {
+      console.log('inside if', parens[key])
+      return false;
+    }
+  }
+  return output;
+}
+
+function parenMapper (arr) {
+  var parenTracker = {};
+  arr.forEach((char) => {
+    parenTracker[char] =  (parenTracker[char] || 0) + 1;
+  });
+  return parenTracker;
 }
 
 module.exports = balancedParens;
