b4454de81c0cbdbec743bd80bb50ee4705651776
Shahrukh <jalilshahrukh@gmail.com>
2018-05-19 09:40:36 -0400

    Binary complete.

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,7 +14,22 @@
  */
 
 function binToDec(binary) {
-
+    const binaryArr = binary.split(''); 
+    let endIndex = binaryArr.length-1; 
+    let binaryDigit = 0;
+    let increasePower = 0;
+  
+    while (endIndex > -1) {  
+      if (binaryArr[endIndex] === '1') {  
+        binaryDigit += Math.pow(2, increasePower);  
+      } else { 
+        binaryDigit += 0; 
+      }//end if else
+      increasePower += 1;
+      endIndex--; 
+    }//end while
+  
+    return binaryDigit; 
 }
 
 module.exports = binToDec;
