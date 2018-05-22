19cc732103217ed36a3ed031c4e3a489b8795f2f
Yan Yan Huang <yanyan.huang15@gmail.com>
2018-05-22 10:01:59 -0400

    working on the punctuation between the letters of reversals

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -10,8 +10,38 @@
 // matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw');  -> true
 // matchWord('');  -> true
 
+// space or punctuation
+// fi if
+// done , for !! for && rof
+
 function matchWord(str) {
+    console.log(str);
+    let amLetter = /[A-Z]/i;
+    let space = /\n/i;
+    
+
+    let firstMatch = amLetter.exec(str).index;
+    console.log(firstMatch);
+    let arrStr = str.split('');
+    console.log(arrStr);
+    arrStr = arrStr.splice(firstMatch).reverse().join('');
+    let secondMatch = amLetter.exec(arrStr).index
+    console.log(secondMatch);
+    let otherHalf = arrStr.split('').splice(secondMatch);
+    console.log(otherHalf);
+
+
+
+    return otherHalf.reverse() === otherHalf;
+    
 
 }
+
+console.log(matchWord('__END_DNE-----'));
+//console.log(matchWord('__ENDDNE__'));  //-> false       (not separated by a space)
+//console.log(matchWord('IF()()fi[]'));  //-> true        (should be case-insensitive)
+//console.log(matchWord('for__if__rof__fi'));  //-> false     not properly closed. like ( [) ] 
+//console.log(matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw'));  // -> true
+//console.log(matchWord('')); // true
 
 module.exports = matchWord;
