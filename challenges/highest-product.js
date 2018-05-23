fb25cef1c7f01851cccc73ccc4805fdc9e82df51
David DeStefano <david.dest01@gmail.com>
2018-05-23 10:16:01 -0400

    hh

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,22 @@
  */
 
 function highestProduct(array) {
-
+  const sorted = array.sort((a, b) => a - b);
+  const greatestVal = sorted[sorted.length -1];
+  var high = 0;
+  for(var i = 0; i < sorted.length - 2; i++) {
+    for(var j = 1; j < sorted.length - 1; j++) {
+      if(i !== j) {
+        temp = sorted[i] * sorted[j];
+        if(high < temp) {
+          high = temp;
+        }
+      }
+    }
+  }
+  return high * greatestVal;
 }
+  
 
 
 module.exports = highestProduct;
