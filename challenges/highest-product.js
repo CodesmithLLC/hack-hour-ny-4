95e7422f1492ee61412bc12084fdf6a910619f66
Shafayat Alam <shafayatalam05@gmail.com>
2018-05-23 09:45:37 -0400

    completed Subset_Sum Challenge

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,24 @@
  */
 
 function highestProduct(array) {
-
+  if (array.length === 0) {
+    return undefined;
+  
+  } else if (array.length <=3) {
+    let product1 = 1;
+    array.forEach( (num) => {
+      product1 = product1 * num;
+    })
+    return product1;
+  
+  } else {
+    let sortedArr = array.sort((a,b) => a - b);
+    let product2 = 1;
+    for (let i = sortedArr.length-1; i >= sortedArr.length - 3; i--) {
+      product2 *= sortedArr[i];
+    }
+    return product2;
+  }
 }
-
 
 module.exports = highestProduct;
