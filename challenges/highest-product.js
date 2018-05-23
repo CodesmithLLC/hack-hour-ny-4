d950e20137585e66b130cd65c589e5e7d3b1c961
Yan Yan Huang <yanyan.huang15@gmail.com>
2018-05-23 09:33:00 -0400

    solution

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,25 @@
  */
 
 function highestProduct(array) {
+    // console.log(array);
+    array = array.map((num) => {
+        return Math.abs(num);
+    }).sort();
+
+    // console.log(array);
+    let product1 = array.pop();
+    // console.log(product1);
+    let product2 = array.pop();
+    // console.log(product2);
+    // console.log(array);
+
+    return product1 * product2;
 
 }
 
+// console.log(highestProduct([-4, -3, -2]));
+// console.log(highestProduct([-6, 2, 1]));
+// console.log(highestProduct([2, 5, -3]));
+// console.log(highestProduct([0, -1, 3]));
 
 module.exports = highestProduct;
