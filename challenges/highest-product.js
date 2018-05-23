1dd4fda7441addbfd89a4c3ccf21cb56f65d7167
Shafayat Alam <shafayatalam05@gmail.com>
2018-05-23 11:00:44 -0400

    Refactored highest product challenge

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -15,13 +15,14 @@ function highestProduct(array) {
   
   } else {
     let sortedArr = array.sort((a,b) => a - b);
+    
     let product2 = 1;
     let len = sortedArr.length;
     let first2 = sortedArr[0] * sortedArr[1];
     let last2 = sortedArr[len - 2] * sortedArr[len - 1];
    
     if ( first2 > last2) {
-      product2 = first2 * sortedArr[2];
+      product2 = first2 * sortedArr[len - 1];
     } else {
       product2 = last2 * sortedArr[len - 3];
     }
