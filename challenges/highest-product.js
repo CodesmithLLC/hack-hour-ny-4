30f832b30cb00ffee1a684ad4f3f695302c60ef4
Wontae Han <wontaeh@gmail.com>
2018-05-23 10:15:46 -0400

    Update highestProduct
    Could handle negative numbers

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -14,13 +14,43 @@
 //   return result;
 // }
 
+// function highestProduct(array) {
+//   if(!Array.isArray(array)) return 0;
+// 	if(array.length < 3) return 0;
+//   return array.sort((a, b) => {
+//     return a-b;
+//   }).reduce((acc, curr, idx)=> {
+// 		if(typeof curr !== 'number') acc = 0;
+//     if(idx >= array.length - 1 - 2) { return acc *= curr; } 
+//     else { return acc; }
+// 	}, 1);
+// }
+
 function highestProduct(array) {
-  return array.sort((a, b) => {
+	if(!Array.isArray(array)) return 0;
+	if(array.length < 3) return 0;
+  array.sort((a, b) => {
     return a-b;
-  }).reduce((acc, curr, idx)=> {
-    if(idx >= array.length - 1 - 2) { return acc *= curr; } 
-    else { return acc; }
-	}, 1);
+  });
+	let negArr = array.filter((elem)=>{return elem < 0});
+	let posArr = array.filter((elem)=>{return elem > 0});
+	if(negArr.length >= 2 && posArr.length >= 2) {
+		if(negArr[0]*negArr[1] > posArr[posArr.length-1]*posArr[posArr.length-2]) {
+			return negArr[0]*negArr[1]*posArr[posArr.length-1];
+		} else {
+			return array.reduce((acc, curr, idx)=> {
+				if(typeof curr !== 'number') acc = 0;
+				if(idx >= array.length - 1 - 2) { return acc *= curr; } 
+				else { return acc; }
+			}, 1);
+		}
+	} else {
+		return array.reduce((acc, curr, idx)=> {
+			if(typeof curr !== 'number') acc = 0;
+			if(idx >= array.length - 1 - 2) { return acc *= curr; } 
+			else { return acc; }
+		}, 1);
+	}
 }
 
 
