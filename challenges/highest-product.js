ab1dc5d98c565a48bf1d105c8b2cf496b392bcb2
root <root@DESKTOP-5P1J73P.localdomain>
2018-05-24 10:32:45 -0400

    fixed variable naming issue - now completed

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -4,14 +4,13 @@
 
 // let arr = [5, 4, 4, 5, 6, 7, 8, 10, -10, -10, -10];
 function highestProduct(array) {
-    arr = arr.sort((a, b) => a - b);
-    // console.log(arr);
-    if (arr.length < 3 || !Array.isArray(arr)) return 0;
-    let prodLast3 = arr[arr.length - 1] * arr[arr.length - 2] * arr[arr.length - 3];
-    let prodFirst2Last = arr[0] * arr[1] * arr[arr.length - 1];
-    return Math.max(prodFirst2Last, prodLast3);
+  array = array.sort((a, b) => a - b);
+  let length = array.length;
+  if (array.length < 3 || !Array.isArray(array)) return 0;
+  let prodLast3 = array[length - 1] * array[length - 2] * array[length - 3];
+  let prodFirst2Last = array[0] * array[1] * array[length - 1];
+  return Math.max(prodFirst2Last, prodLast3);
 }
 // console.log(highestProduct(arr));
-
 
 module.exports = highestProduct;
