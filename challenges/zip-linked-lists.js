505427518071629e515f406cd48ccf283d2642f7
root <root@DESKTOP-5P1J73P.localdomain>
2018-05-24 10:21:15 -0400

    completed

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,16 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
-};
+  if (!l1) return l2;
+  let head = new Node();
+  let head = l1;
+  while (l2) {
+    head = l1.next;
+    l1.next = l2;
+    l1 = l2;
+    l2 = head;
+  }
+  return head;
+}
 
-module.exports = {Node: Node, zip: zip};
+module.exports = { Node: Node, zip: zip };
