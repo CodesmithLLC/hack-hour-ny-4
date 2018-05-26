fb332cea9c7cc513c43e39aebbf623fcea3234c7
Shafayat Alam <shafayatalam05@gmail.com>
2018-05-26 10:00:18 -0400

    completed max-stack with 0(n) solution

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -8,6 +8,41 @@
 
 function Stack() {
   // body...
+  this.length = 0;
+  this.storage = {};
+  
 }
 
+Stack.prototype.push = function (val) {
+  this.storage[this.length] = val;
+  this.length++;
+  return this.length;
+}
+
+Stack.prototype.pop = function () {
+  let poppedVal = this.storage[this.length - 1];
+  delete this.storage[this.length - 1];
+  this.length --;
+  return poppedVal;
+}
+
+Stack.prototype.getMax = function () {
+  let maxVal = -Infinity;
+  for (let val in this.storage) {
+    if (this.storage[val] > maxVal) maxVal = this.storage[val];
+  }
+
+  return maxVal;
+}
+
+// let myStack = new Stack();
+
+// console.log(myStack.push(2));
+// console.log(myStack.push(-2));
+// console.log(myStack.push(5));
+// console.log(myStack.push(3));
+// console.log(myStack.pop());
+// console.log(myStack.getMax());
+
 module.exports = Stack;
+
