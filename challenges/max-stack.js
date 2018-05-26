7ba2b012f9678ee9bdfe6e9a5bb90e2c498593b2
Wontae Han <wontaeh@gmail.com>
2018-05-26 09:57:25 -0400

    Complete max-stack.js except BONUS

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,29 @@
  */
 
 function Stack() {
-  // body...
+  this.storage = {};
+  this.index = 0;
+}
+Stack.prototype.push = (value) => {
+  this.index++;
+  this.storage[this.index] = value;
+  return this.index;
+}
+Stack.prototype.pop = () => {
+  let removed = this.storage[this.index-1];
+  delete this.storage[this.index-1];
+  this.index--;
+  if(this.index < 0) this.index = 0;
+  return removed;
+}
+Stack.prototype.getMax = () => {
+  let maxValue = 0;;
+  for(let key in this.storage) {
+    if(this.storage[key] > maxValue) {
+      maxValue = this.storage[key];
+    }
+  }
+  return maxValue;
 }
 
 module.exports = Stack;
