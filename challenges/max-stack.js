26478217de3529c4f80bdd1e17444330e982f48a
Zach Franz <zachfranz@Zachs-MBP.EAP1750H8>
2018-05-26 09:50:59 -0400

    completed maxStack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,26 @@
  */
 
 function Stack() {
-  // body...
+	const stackObj = {};
+	stackObj.arr = [];
+	stackObj.maxVal = undefined;
+
+	stackObj.push = function (val) {
+		if (stackObj.maxVal === undefined || stackObj.maxVal < val) stackObj.maxVal = val;
+		return stackObj.arr.push(val);
+	};
+
+	stackObj.pop = function () {
+		let popVal = stackObj.arr.pop();
+		if (popVal === stackObj.maxVal) stackObj.maxVal = Math.max(...stackObj.arr);
+		return popVal;
+	};
+
+	stackObj.getMax = function () {
+		return stackObj.maxVal;
+	};
+
+	return stackObj;
 }
 
 module.exports = Stack;
