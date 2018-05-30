cd74d0d16ab5e5173e9d589b3ff5232eb6a35fb7
Zach Franz <zachfranz@Zachs-MacBook-Pro.local>
2018-05-30 09:54:13 -0400

    completed reverse linked list

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -9,12 +9,26 @@
  */
 
 function Node(value) {
-    this.value = value;
-    this.next = null;
+  this.value = value;
+  this.next = null;
 }
 
 function reverseLinkedList(head) {
+  const refArr = [];
+  let curNode = head.next;
 
+  while (curNode !== null) {
+    refArr.push(curNode);
+    curNode = curNode.next;
+  }
+
+  for (let i = 0; i < refArr.length; i += 1) {
+    if (i === 0) refArr[i].next = head;
+    else refArr[i].next = refArr[i - 1];
+  }
+  head.next = null;
+
+  return refArr.length === 0 ? head : refArr[refArr.length - 1];
 }
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
