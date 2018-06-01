29f1dc06e9c8dc3702a1284e06138ca96327feb1
Wontae Han <wontaeh@gmail.com>
2018-06-01 09:31:22 -0400

    Complete mergeArrays and Update apple-stocks

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -14,7 +14,21 @@
  */
 
 function mergeArrays(arr1, arr2) {
-
+  let result = [];
+  while(arr1.length && arr2.length) {
+    if(arr1[0] <= arr2[0]) {
+      result.push(arr1.shift());
+    } else {
+      result.push(arr2.shift());
+    }
+  }
+  if(arr1.length) {
+    result.push(...arr1);
+  }
+  if(arr2.length) {
+    result.push(...arr2)
+  }
+  return result;
 }
 
 module.exports = mergeArrays;
