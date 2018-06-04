5312ac3b143e9d1b06c14fba8136fc85fa39d3ab
Zach Franz <zachfranz@Zachs-MacBook-Pro.local>
2018-06-04 10:11:00 -0400

    completed perm palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,24 @@
  */
 
 function permPalin(str) {
-	
+  const isPalin = (str) => str === str.split('').reverse().join('');
+
+  const getPerm = (str) => {
+    let permArr = [];
+    let strArr = str.split('');
+
+    let addPermElem = (str, letters) => {
+      if (letters.length === 1) return permArr.push(str += letters[0]);
+      letters.forEach((letterElem, letterInd, letterArr) => {
+        addPermElem(str + letterElem, letterArr.filter((filterElem, filterInd) => filterInd !== letterInd));
+      });
+    };
+
+    addPermElem('', strArr);
+    return permArr;
+  };
+
+  return getPerm(str).some((str) => isPalin(str));
 }
 
 module.exports = permPalin;
