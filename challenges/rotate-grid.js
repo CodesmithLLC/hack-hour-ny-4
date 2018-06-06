7d17d514b90cf4278a01f9a05e67fe717976d051
Wontae Han <wontaeh@gmail.com>
2018-06-06 09:59:20 -0400

    Complete stack-queue

--- challenges/rotate-grid.js
+++ challenges/rotate-grid.js
@@ -16,7 +16,30 @@
  * BONUS: Do this in place
  */
 
+ // 1,2,3, 4,5,6, 7,8,9
+ // 4,1,2, 7,5,3, 8,9,6
+ // 7,4,1, 8,5,2, 9,6,3
+ // 8,7,4, 9,5,1, 6,3,2
+
+ // 1,2,3, 6, 9,8,7, 4
+ // 4,1,2, 3, 6,9,8, 7
+ // 7,4,1, 2, 3,6,9, 8
+
 function rotateGrid(grid, n) {
+  // let newArr = [...grid[0]];
+  // for(let i = 1; i < grid.length-1; i++) {
+  //   let row = grid[i];
+  //   newArr.push( row[row.length-1] );
+  // }
+  // newArr.push(...grid[grid.length-1].reverse());
+  // for(let i = grid.length-1-1; i > 0; i--) {
+  //   let row = grid[i];
+  //   newArr.push( row[0] ); 
+  // }
+
+  // for(let i = 1; i < n; i++) {
+  //   newArr.unshift(newArr.pop());
+  // }
 
 }
 
--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,17 +4,41 @@
 
 
 function Stack() {
-
+  this.storage = [];
 }
-
+Stack.prototype.push = function(val) {
+  this.storage.push(val);
+}
+Stack.prototype.pop = function() {
+  return this.storage.pop();
+}
 
 /**
 * Queue Class
 */
 
 
-function Queue() {
 
+function Queue() {
+  this.enqueueBox = new Stack();
+  this.dequeueBox = new Stack();
+}
+
+Queue.prototype.enqueue = function(val){
+  this.enqueueBox.push(val);
+}
+
+Queue.prototype.dequeue = function(){
+  let result = null;
+  if(!this.dequeueBox.storage.length) {
+    while(this.enqueueBox.storage.length) {
+      this.dequeueBox.push(this.enqueueBox.pop());
+    }
+  }
+  if(this.dequeueBox.storage.length) {
+    result = this.dequeueBox.pop();
+  }
+  return result;
 }
 
 module.exports = {Stack: Stack, Queue: Queue};
