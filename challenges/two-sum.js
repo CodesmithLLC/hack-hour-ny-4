9f6b09f159a84dec1a03d5f1823eaf1cf380d563
Alejandro R <Alexrom789@Gmail.com>
2018-06-07 09:47:56 -0400

    attempted two-sum hack hour

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,21 @@
  */
 
 function twoSum(arr, n) {
+for (let i = 0; i <= arr.length; i++){
+    for (let j = i + 1; j < arr.length; j++)
+    {
+        //console.log('Arr i: ' , arr[i]);
+        //console.log('Arr j + 1: ', arr[j]);
+        if (arr[i] + arr[j] === n){
+            return true;
+        }
+    }
 
 }
 
+return false;
+}
+
 module.exports = twoSum;
+
+//console.log(twoSum([2, 3, 5], 8)); 
