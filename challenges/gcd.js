9d1c1e1d38f187f75e2e58e02cd9fec29bc9bf3f
Wontae Han <wontaeh@gmail.com>
2018-06-18 09:29:46 -0400

    Complete gcd.js

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,13 @@
  */
 
 function gcd(a, b) {
-
+  let divisor = Math.max(a, b);
+  while(divisor > 1) {
+    if( a % divisor === 0 && b % divisor === 0) return divisor;
+    divisor--;
+  }
+  if(divisor < 0) divisor = 1;
+  return divisor;
 }
 
 module.exports = gcd;
