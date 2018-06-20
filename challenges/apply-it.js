d82901f195bfea992a5d7f94012af4a3dc4fbbbb
Rebecca Leff <rebeccaleff@Rebeccas-MacBook-Pro.local>
2018-06-20 09:25:11 -0400

    Hack Hours to June 20

--- challenges/apply-it.js
+++ challenges/apply-it.js
@@ -26,7 +26,27 @@
  */
 
 function applyIt(func, args) {
-
+  if(args.length === 3) {
+    console.log('inside args length 3 ');
+    func(args[0], args[1], args[2]);
+  } else if (args.length === 2) {
+    console.log('inside args length 2 ');
+    func(args[0], args[1]);
+  } else if (args.length === 1) {
+    console.log('inside args length 1 ');
+    func(args[0]);
+  }
 }
+
+var jasmine = function(name, age) {
+    if(!age){
+      return "We don't know how old " + name + " is!";
+    }
+    else{
+      return name + " is " + age + " years old!";
+    }
+};
+
+console.log(applyIt(jasmine, ["Jasmine"]));
 
 module.exports = applyIt;
