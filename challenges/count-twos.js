c3d67fe7fb53c7a0b3db010c97eac0950a70505c
Zach Franz <zachfranz@Zachs-MacBook-Pro.local>
2018-06-20 10:29:58 -0400

    adding count-twos

--- challenges/count-twos.js
+++ challenges/count-twos.js
@@ -1,6 +1,6 @@
 // Write a function "countTwos" that takes a number as an argument, n. The function will return the number of 2s encountered when counting from 1 to n (inclusive). 
 // For example: countTwos(3) should be 1 because there are no 2s in the number one. There is One 2 in the number two and no 2s in the number three.
-// Each 2 should be counted. For instance,  the number 2222 has four 2s. 
+// Each 2 should be counted. For instance,  the number 2222 has four 2s.
 
 // countTwos(1);  -> 0
 // countTwos(3);  -> 1
@@ -10,7 +10,13 @@
 
 
 function countTwos(num) {
-
+  let numTwos = 0;
+  let matchs;
+  for (let i = num; i > 0; i -= 1) {
+    matchs = i.toString().match(/2/g);
+    if (matchs !== null) numTwos += matchs.length;
+  }
+  return numTwos;
 }
 
 module.exports = countTwos;
