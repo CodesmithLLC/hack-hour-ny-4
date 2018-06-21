895e16c7e4e8bb81ec8afb27bee19904f1cc8615
Wontae Han <wontaeh@gmail.com>
2018-06-21 09:51:04 -0400

    Complete add-linked-list

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -5,7 +5,7 @@
  * EXAMPLE
  * Input: (2 -> 1 -> 5) + (5 -> 9 -> 2)
  * Output: 7 -> 0 -> 8
- *
+ * 512 + 295 = 807
  * The 7 is the ones digit (2 + 5).
  * The 0 is the tens digit (1 + 9, carry the 1).
  * The 8 is the hundreds digit (1 carried over + 5 + 2).
@@ -18,7 +18,31 @@ function Node(val) {
 }
 
 function addLinkedList(l1, l2) {
+  let carry = 0;
+  let newNode = new Node(null);
+	let result = newNode;
+  let currL1 = l1;
+  let currL2 = l2;
+  while(currL1 && currL2) {
+    let addVal = currL1.value + currL2.value + carry;
+    if(addVal >= 10) {
+      addVal = addVal - 10;
+      carry = 1;
+    } else {
+      carry = 0;
+    }
+    newNode.value = addVal;
 
+    currL1 = currL1.next;
+    currL2 = currL2.next;
+		if(currL1 && currL2) {
+			newNode.next = new Node(null);
+			newNode = newNode.next;
+		}
+  }
+  if(currL1) newNode.next = currL1;
+  if(currL2) newNode.next = currL2;
+  return result;
 }
 
 module.exports = {Node: Node, addLinkedList: addLinkedList};
