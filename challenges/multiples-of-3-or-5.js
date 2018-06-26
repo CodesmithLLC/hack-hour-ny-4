a8caa60021a7f51b2341066b6181a8e70e304ec1
Wontae Han <wontaeh@gmail.com>
2018-06-26 09:43:15 -0400

    Complete multiples-of-3-or-5

--- challenges/multiples-of-3-or-5.js
+++ challenges/multiples-of-3-or-5.js
@@ -7,7 +7,19 @@
 
 function sumMultiples3Or5Below1000() {
   let sum = 0;
-
+  // multiples of 3 below 1000
+  let multiples = [];
+  for(let i = 1, j = 3; j < 1000; i++, j = i * 3) {
+    multiples.push(j);
+  }
+  // multiples of 5 below 1000
+  for(let i = 1, j = 5; j < 1000; i++, j = i * 5) {
+    multiples.push(j);
+  }
+  // sum
+  for(let i = 0; i < multiples.length; i++) {
+    sum += multiples[i];
+  }
   return sum;
 }
 
@@ -16,9 +28,23 @@ function sumMultiples3Or5Below1000() {
 // and returns the sum of multiples of x and y below z
 function sumMultiplesXOrYBelowZ(x, y, z) {
   let sum = 0;
-
+  // multiples of x below z
+  let multiples = [];
+  for(let i = 1, j = x; j < z; i++, j = i * x) {
+    multiples.push(j);
+  }
+  // multiples of y below z
+  for(let i = 1, j = y; j < z; i++, j = i * y) {
+    multiples.push(j);
+  }
+  // sum
+  for(let i = 0; i < multiples.length; i++) {
+    sum += multiples[i];
+  }
   return sum;
 }
+
+
 
 const objectToExport = {
   sumMultiples3Or5Below1000,
