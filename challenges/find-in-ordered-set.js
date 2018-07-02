17163aa9fa22f0d97ed07c7434f2b91b9630a62c
Zach Franz <zachfranz@Zachs-MacBook-Pro.local>
2018-07-02 10:29:25 -0400

    completed find in ordered set

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -11,7 +11,25 @@ findInOrderedSet(nums, 2);  -> false
 
 
 function findInOrderedSet(arr, target) {
+  let ind = Math.floor(arr.length / 2);
+  let prevMaxInd = arr.length - 1;
+  let prevMinInd = 0;
 
+  while (true) {
+    if (target === arr[ind]) return true;
+
+    if (ind === prevMaxInd || ind === prevMinInd) return false;
+
+    else if (target < arr[ind]) {
+      if (ind === 0) return false;
+      prevMaxInd = ind;
+      ind = Math.floor((ind + prevMinInd) / 2);
+    } else if (target > arr[ind]) {
+      if (ind === arr.length - 1) return false;
+      prevMinInd = ind;
+      ind = Math.ceil((ind + prevMaxInd) / 2);
+    }
+  }
 }
 
 
