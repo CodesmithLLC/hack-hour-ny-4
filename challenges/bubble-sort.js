4fba477d02fafe1a9084a640ea87cc158687257a
Zach Franz <zachfranz@Zachs-MacBook-Pro.local>
2018-07-09 09:22:56 -0400

    completed bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -3,9 +3,20 @@
 // https://en.wikipedia.org/wiki/Bubble_sort
 
 
-
 function bubbleSort(array) {
+  let sorted = false;
 
+  while (!sorted){
+    sorted = true;
+    for (let i = 0 ; i < array.length - 1; i += 1) {
+      if (array[i] > array[i + 1]){
+        [array[i], array[i + 1]] = [array[i + 1], array[i]];
+        sorted = false;
+      }
+    }
+  }
+
+  return array;
 }
 
 module.exports = bubbleSort;
