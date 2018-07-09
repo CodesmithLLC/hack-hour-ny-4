3ee4e5ceac716b7c77ba2f619fb41a312f589b30
Alejandro R <Alexrom789@Gmail.com>
2018-07-09 09:43:04 -0400

    Completed bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,25 @@
 
 
 function bubbleSort(array) {
+    let sorted;
+    do {
+        sorted = false;
+    for (let i = 0; i < array.length; i++){
+        let smaller;
+        let larger;
+        if (array[i] > array[i +1]){
+           smaller = array[i + 1];
+           larger = array[i] 
+           array[i] = smaller;
+           array[i + 1] = larger;
+           sorted = true;
+        }
+    }
+} while (sorted)
+    // console.log(array);
 
 }
 
 module.exports = bubbleSort;
+
+// bubbleSort([7,3,5,2,8,1]);
